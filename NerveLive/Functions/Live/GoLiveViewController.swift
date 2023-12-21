2deae700b31519ba7cb82c0b96f8e75a1306061a
王保湘 <zhongdaima@163.com>
2023-12-21 09:58:29 +0800

    feat:  修改内容提交

--- NerveLive/Functions/Live/GoLiveViewController.swift
+++ NerveLive/Functions/Live/GoLiveViewController.swift
@@ -19,13 +19,15 @@ class GoLiveViewController: BaseViewController {
     }
 
     lazy var openLiveBtn: UIButton = {
-        let width = (374.0 * 61.0) / 183.0
+        //let width = (374.0 * 61.0) / 183.0
+        let width = (700.0 * 61.0) / 183.0
         let openLiveBtn = UIButton(frame: CGRect(x: (K_SCREEN_WIDTH - width) / 2.0, y: (K_SCREEN_HEIGHT - 61) / 2.0, width: width, height: 61))
         openLiveBtn.backgroundColor = .clear
         //openLiveBtn.setTitle("Go Live", for: .normal)
-        //openLiveBtn.titleLabel?.textAlignment = .center
-        //openLiveBtn.titleLabel?.font = UIFont.font(ofSize: 36, type: .Bold)
-        openLiveBtn.setImage(UIImage(named: "icon_go_live"), for: .normal)
+        openLiveBtn.setTitle("Stream Now", for: .normal)
+        openLiveBtn.titleLabel?.textAlignment = .center
+        openLiveBtn.titleLabel?.font = UIFont.font(ofSize: 36, type: .Bold)
+        //openLiveBtn.setImage(UIImage(named: "icon_go_live"), for: .normal)
         openLiveBtn.addTarget(self, action: #selector(goLive), for: .touchUpInside)
         return openLiveBtn
     }()
