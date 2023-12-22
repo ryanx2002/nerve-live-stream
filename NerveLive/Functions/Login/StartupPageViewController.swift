4a479bcc2ea30770636d9c76f0d3bfda9c6f2ee2
Thomas Wang <wang.thomasdavid@gmail.com>
2023-12-21 23:04:33 -0500

    finished ui changes to disclaimer text on start page

--- NerveLive/Functions/Login/StartupPageViewController.swift
+++ NerveLive/Functions/Login/StartupPageViewController.swift
@@ -49,37 +49,36 @@ class StartupPageViewController: BaseViewController {
     // MARK: 隐私条款 服务条款
     private lazy var agreeLabel: YYLabel = {
         let style = NSMutableParagraphStyle()
-        style.lineSpacing = 5
+        style.lineSpacing = 2
         style.alignment = .center
         let text = "By tapping “Play”, you’re accepting the Terms and Privacy Policy.\nClick “Terms” or “Privacy Policy” to view them."
         let content = NSMutableAttributedString(string: text)
-        content.yy_setFont(FONT(12), range: NSRange(location: 0, length: text.count))
+        content.yy_setFont(FONT(8), range: NSRange(location: 0, length: text.count))
         content.yy_setParagraphStyle(style, range: NSRange(location: 0, length: text.count))
         content.yy_setColor(.white, range: NSRange(location: 0, length: text.count))
 
         let range1: NSRange = text.nsRange(from: text.range(of: "“Terms”")!)
-        content.yy_setTextHighlight(range1, color: RGBA(r: 0, g: 255, b: 6, a: 1), backgroundColor: nil) { (_, _, _, _) in
+        content.yy_setTextHighlight(range1, color: .white, backgroundColor: nil) { (_, _, _, _) in
             let vc = WebViewController()
             vc.urlString = "https://sites.google.com/view/nerve-terms-of-service/home"
             self.present(vc, animated: true)
         }
 
         let range2: NSRange = text.nsRange(from: text.range(of: "“Privacy Policy”")!)
-        content.yy_setTextHighlight(range2, color: RGBA(r: 0, g: 255, b: 6, a: 1), backgroundColor: nil) { (_, _, _, _) in
+        content.yy_setTextHighlight(range2, color: .white, backgroundColor: nil) { (_, _, _, _) in
             let vc = WebViewController()
             vc.urlString = "https://sites.google.com/view/nerve-privacy-policy/home"
             self.present(vc, animated: true)
         }
 
-        let label = YYLabel(frame: CGRect(x: 0, y: playBtn.frame.maxY + 18, width: K_SCREEN_WIDTH, height: 20))
+        let label = YYLabel(frame: CGRect(x: 0, y: playBtn.frame.maxY + 18, width: K_SCREEN_WIDTH, height: 40))
         label.backgroundColor = .clear
-        label.font = FONT(12)
+        label.font = FONT(8)
         label.textColor = .white
         label.numberOfLines = 0
         label.textVerticalAlignment = .center
         label.textAlignment = .center
         label.attributedText = content
-        label.sizeToFit()
         return label
     }()
 }
